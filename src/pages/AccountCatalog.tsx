8e1ec71b62bde358d770a8f6ee66734b42c310c2
ruandd9 <ruandd9@gmail.com>
2025-04-15 09:40:30 -0300

    ajusta css da pagina de catalogos

--- src/pages/AccountCatalog.tsx
+++ src/pages/AccountCatalog.tsx
@@ -455,7 +455,7 @@ const AccountCatalog: React.FC = () => {
             {/* Botão de Anunciar Conta */}
             <button
               onClick={() => setIsAnunciarModalOpen(true)}
-              className="lg:absolute lg:right-4 lg:top-4 px-4 py-2 bg-gradient-to-r from-liquid-teal to-liquid-blue text-white font-medium rounded-md shadow-lg shadow-liquid-teal/20 hover:shadow-liquid-teal/40 transition-all transform hover:-translate-y-1 flex items-center"
+              className="lg:absolute lg:right-4 lg:top-4 px-2 py-2 bg-gradient-to-r from-liquid-teal to-liquid-blue text-white font-medium rounded-md shadow-lg shadow-liquid-teal/20 hover:shadow-liquid-teal/40 transition-all transform hover:-translate-y-1 flex items-center mb-5"
             >
               <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
